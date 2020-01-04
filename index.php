dc7812024303f31a0e188024c24a5c414ecf8525
Cristiano Simao <40866219+CristianoSimao@users.noreply.github.com>
2020-01-04 18:57:15 -0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+	//Here we create some variables to see the result in the debug screen.
+	session_start();
+	SESSION["variable1"]="dog";
+	SESSION["variable2"]="cat";
+	SESSION["variable3"]="fox";
+	SESSION["variable4"]= 245;
+	SESSION["variable5"]= 333;
+?>
+<!DOCTYPE html>
+<html lang="pt">
+<head>
+	<meta charset="utf-8">
+	<title>Simple test page</title>
+</head>
+<body>
+<h2>For see the debug screen enable popup and javascript in this page in your browser. Reload this page with the GET variable debug=ligado in the url </h2>
+<?php
+	include "ui_debug.php";
+?>
+</body>
+</html>
